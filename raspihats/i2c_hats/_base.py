5a5f2d154ba5004157d496cf5ee894161203ec35
Florin Costa <florin.costa83@gmail.com>
2017-02-05 22:52:23 +0200

    replaced double leading underscore for non public methods and attributes with single underscore

--- raspihats/i2c_hats/_base.py
+++ raspihats/i2c_hats/_base.py
@@ -8,26 +8,26 @@ import threading
 from ._frame import I2CFrame
 
 class Command(object) :
-    """I2C-HAT commands""" 
-    
+    """I2C-HAT commands"""
+
     # common board commands
     GET_BOARD_NAME = 0x10
     GET_FIRMWARE_VERSION = 0x11
     GET_STATUS_WORD = 0x12
     RESET = 0x13
-    
+
     # Communication WatchDog commands
     CWDT_SET_PERIOD = 0x14
     CWDT_GET_PERIOD = 0x15
     CWDT_SET_STATE = 0x16
-    
+
     # Digital Inputs commands
     DI_GET_ALL_CHANNEL_STATES = 0x20
     DI_GET_CHANNEL_STATE = 0x21
     DI_GET_COUNTER = 0x22
     DI_RESET_COUNTER = 0x23
     DI_RESET_ALL_COUNTERS = 0x24
-    
+
     # Digital Outputs commands
     DO_SET_POWER_ON_VALUE = 0x30
     DO_GET_POWER_ON_VALUE = 0x31
@@ -43,71 +43,71 @@ class I2CHatResponseException(Exception):
 
 class I2CHat(object):
     """Implements basic functionality common to all I2C-HATs.
-    
+
     Args:
         address (int): I2C bus address, valid range depends of base_address
         base_address (int): I2C-HAT family starting address
         board_name (str): I2C-HAT expected board name
-        
+
     Raises:
         ValueError: If address is not in range
-        
-    (*) - attribute value read directly from I2C-HAT 
-        
+
+    (*) - attribute value read directly from I2C-HAT
+
     """
-        
+
     _i2c_bus_lock = threading.Lock()
     _i2c_bus = None
     try:
         _i2c_bus = smbus.SMBus(1)     # default for Raspberry Pi
     except:
         print("I2C port not found!")
-    
+
     def __init__(self, address, base_address=None, board_name=None):
-        self.__address = address
-        self.__frame_id = 0x1F - 1
-        self.__transfer_time = None
-        
+        self._address = address
+        self._frame_id = 0x1F - 1
+        self._transfer_time = None
+
         if base_address == None:
             if not 0 <= address <= 127:
                 raise ValueError("I2C address should be in range[0, 127]")
         else:
             if address & base_address != base_address:
                 raise ValueError("I2C address should be in range[" + hex(base_address) + ", " + hex(base_address + 0x0F) + "]")
-            
+
         if board_name != None:
             if self.name not in board_name:
                 raise Exception("Unexpected board name: " + self.name + ", expecting: " + board_name)
-        
+
     def __str__(self):
-        return self.name + " adr: " + hex(self.__address)
+        return self.name + " adr: " + hex(self._address)
 
     def _generate_frame_id_(self):
         """Generate new frame Id, increments current frame Id, wraps to 0xFF.
-        
+
         Returns:
             int: The new frame Id
-            
+
         """
-        self.__frame_id += 1
-        self.__frame_id &= 0xFF
-        return self.__frame_id
-    
+        self._frame_id += 1
+        self._frame_id &= 0xFF
+        return self._frame_id
+
     def _transfer_(self, request_frame, response_data_size, response_expected = True, number_of_tries = 5):
         """Tries a number of times to send a request frame and to get a response frame over I2C bus.
-        
+
         Args:
             request_frame (I2CFrame): Request frame to be sent over the I2C bus
             response_data_size (int): Expected response data size, this is the payload data size
             response_expected (bool): True if a respose is expected
             number_of_tries (int): Number of tries to get the response
-            
+
         Returns:
             I2CFrame: The response frame
-            
+
         Raises:
             I2CHatResponseException: After all attempts to get a response have failed
-            
+
         """
         with I2CHat._i2c_bus_lock:
             exceptions = []
@@ -116,24 +116,24 @@ class I2CHat(object):
                 try:
                     request_data = request_frame.encode()
                     # print request_data
-                    
+
                     # NOTE: write_i2c_block_data function is used to send commands to the I2C-HAT
-                    I2CHat._i2c_bus.write_i2c_block_data(self.__address, request_data[0], request_data[1:])
-                    
+                    I2CHat._i2c_bus.write_i2c_block_data(self._address, request_data[0], request_data[1:])
+
                     if not response_expected:
                         return
-                     
+
                     # NOTE: read_i2c_block_data function sends a i2c_write first, this write has a length of one, and the dummy_byte as payload, this
-                    # write will be ignored by the I2C-HAT, after this a i2c_read will be issued, this i2c_read is used for reading the response        
+                    # write will be ignored by the I2C-HAT, after this a i2c_read will be issued, this i2c_read is used for reading the response
                     dummy_byte = 0xFF
                     expected_response_size = I2CFrame.ID_SIZE + I2CFrame.CMD_SIZE + response_data_size + I2CFrame.CRC_SIZE
-                    response_data = I2CHat._i2c_bus.read_i2c_block_data(self.__address, dummy_byte, expected_response_size)
+                    response_data = I2CHat._i2c_bus.read_i2c_block_data(self._address, dummy_byte, expected_response_size)
                     # print response_data
-                    
+
                     # build response frame
                     response_frame = I2CFrame(request_frame.id, request_frame.cmd)
                     response_frame.decode(response_data)
-                    self.__transfer_time = time.time()
+                    self._transfer_time = time.time()
                     return response_frame
                 except Exception as ex:
                     exceptions.append(ex)
@@ -145,19 +145,19 @@ class I2CHat(object):
                         for i in range(0, len(exceptions)):
                             message += "try: " + str(i + 1) + " result: " + str(exceptions[i]) + "\n"
                         raise I2CHatResponseException(message)
-                
+
     def _get_u32_value_(self, cmd):
         """Generic get for a unsigned32 value.
-        
+
         Args:
             cmd (int): Command byte value
-        
+
         Returns:
             int: The desired unsigned32 value
-            
+
         Raises:
             I2CHatResponseException: If response has bad data length
-            
+
         """
         request = self._request_frame_(cmd, [])
         response = self._transfer_(request, 4)
@@ -168,14 +168,14 @@ class I2CHat(object):
 
     def _set_u32_value_(self, cmd, value):
         """Generic set for a unsigned32 value.
-        
+
         Args:
             cmd (int): Command byte value
             value (int): Value to be set
-        
+
         Returns:
             int: The desired unsigned32 value
-            
+
         Raises:
             I2CHatResponseException: If response has unexpected format
         """
@@ -187,33 +187,33 @@ class I2CHat(object):
 
     def _request_frame_(self, cmd, data = []):
         """Build request frame, taking care of new frame Id generation.
-        
+
         Args:
             cmd (int): Frame command byte value
-            data (List[int]): Frame payload data 
-        
+            data (List[int]): Frame payload data
+
         Returns:
             I2CFrame: The new I2CFrame built with specified parameters
-            
+
         """
         return I2CFrame(self._generate_frame_id_(), cmd, data)
 
     @property
     def transfer_time(self):
         """:obj:`float`: Last valid transfer time stamp."""
-        return self.__transfer_time
-    
+        return self._transfer_time
+
     @property
     def address(self):
         """:obj:`int`: I2C bus address."""
-        return self.__address
-    
+        return self._address
+
     @property
     def name(self):
         """:obj:`string`: Name(*)."""
         request = self._request_frame_(Command.GET_BOARD_NAME)
         response = self._transfer_(request, 25)
-        board_name = ''    
+        board_name = ''
         for byte in response.data:
             if byte == 0:
                 break;
@@ -227,12 +227,12 @@ class I2CHat(object):
         response = self._transfer_(request, 3)
         data = response.data
         return 'v' + chr(data[0] + 0x30) + '.' + chr(data[1] + 0x30)  + '.' + chr(data[2] + 0x30)
-    
+
     @property
     def status(self):
         """:obj:`int`: Status word(*)."""
         return self._get_u32_value_(Command.GET_STATUS_WORD)
-    
+
     def reset(self):
         """Sends a reset request to the I2C-HAT."""
         request = self._request_frame_(Command.RESET)
@@ -241,57 +241,57 @@ class I2CHat(object):
 
 class I2CHatModule(object):
     """I2C-HAT module base.
-    
+
     Args:
         i2c_hat (:obj:`raspihats._i2c_hat.I2CHat`): I2CHat instance
         labels (:obj:`list` of :obj:`str` or optional): Channel labels
-    
+
     """
-    
+
     def __init__(self, i2c_hat, labels=None):
         self._i2c_hat = i2c_hat
-        self.__labels = labels
+        self._labels = labels
         if labels != None:
-            self.__lc_labels = [l.lower() for l in labels]
-    
+            self._lc_labels = [l.lower() for l in labels]
+
     def _validate_channel_index(self, index):
-        if self.__labels == None:
+        if self._labels == None:
             raise Exception()
-        
+
         label = None
         if isinstance(index, int):
-            if not (0 <= index < len(self.__labels)):
+            if not (0 <= index < len(self._labels)):
                 raise IndexError("'" + str(index) + "' is not a valid channel index")
         elif isinstance(index, str):
             label = index
             try:
-                index = self.__lc_labels.index(label.lower())
+                index = self._lc_labels.index(label.lower())
             except ValueError:
                 raise ValueError("'" + label + "' is not a valid channel label")
         else:
             raise ValueError("index type is '" + type(index) + "', expecting 'int' or 'str'")
         return index
-    
+
     def _validate_value(self, value):
-        max_value = (0x01 << len(self.__labels)) - 1
+        max_value = (0x01 << len(self._labels)) - 1
         if not (0 <= value <= max_value):
             raise ValueError("'" + str(value) + "' is not a valid value, range is [0x00 .. " + hex(max_value) + "]")
-    
+
     @property
     def labels(self):
         """:obj:`list` of :obj:`str`: Channel Labels."""
-        return self.__labels
-            
+        return self._labels
+
 class Cwdt(I2CHatModule):
     """Provides attributes and methods for operating the I2C-HAT CommunicationWatchdogTimer module.
-    
+
     Args:
         i2c_hat (:obj:`raspihats._i2c_hat.I2CHat`): I2CHat instance
-    
+
     (*) - attribute value read directly from I2C-HAT
-    
+
     """
-    
+
     def __init__(self, i2c_hat):
         I2CHatModule.__init__(self, i2c_hat)
 
@@ -305,4 +305,3 @@ class Cwdt(I2CHatModule):
         if value < 0:
             raise ValueError("period should be greather than zero to enable the CommunicationWatchdogTimer on the I2C-HAT board")
         self._i2c_hat._set_u32_value_(Command.CWDT_SET_PERIOD, int(value * 1000))
-
--- raspihats/i2c_hats/_boards.py
+++ raspihats/i2c_hats/_boards.py
@@ -6,43 +6,43 @@ from ._digital import DigitalOutputs, DigitalInputs
 
 def set_i2c_port(i2c_port):
     """Set the I2C port number.
-            
+
     Args:
         i2c_port (int): I2C port number
-    
+
     """
     import smbus
     I2CHat._i2c_bus = smbus.SMBus(i2c_port)
 
 class Di16(I2CHat):
     """This class exposes all operations supported by the Di16 I2C-HAT.
-    
+
     Args:
         address (:obj:`int`): I2C bus address, valid range is [0x40, 0x4F]
 
     Attributes:
         cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
         di (:obj:`raspihats._digital.DigitalInputs`): provides access to DigitalInputs.
-    
+
     """
 
-    __BASE_ADDRESS = 0x40
-    __BOARD_NAME = 'Di16 I2C-HAT'
-    __labels = [
+    _BASE_ADDRESS = 0x40
+    _BOARD_NAME = 'Di16 I2C-HAT'
+    _labels = [
         'Di1.1', 'Di1.2', 'Di1.3', 'Di1.4',
         'Di2.1', 'Di2.2', 'Di2.3', 'Di2.4',
         'Di3.1', 'Di3.2', 'Di3.3', 'Di3.4',
         'Di4.1', 'Di4.2', 'Di4.3', 'Di4.4',
     ]
-    
+
     def __init__(self, address):
-        I2CHat.__init__(self, address, self.__BASE_ADDRESS, self.__BOARD_NAME)
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
         self.cwdt = Cwdt(self)
-        self.di = DigitalInputs(self, self.__labels)
+        self.di = DigitalInputs(self, self._labels)
 
 class Rly10(I2CHat):
     """This class exposes all operations supported by the Rly10 I2C-HAT.
-    
+
     Args:
         address (:obj:`int`): I2C bus address, valid range is [0x50, 0x5F]
 
@@ -50,19 +50,19 @@ class Rly10(I2CHat):
         cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
         do (:obj:`raspihats._digital.DigitalOutputs`): provides access to DigitalOutputs.
     """
-    
-    __BASE_ADDRESS = 0x50    
-    __BOARD_NAME = 'Rly10 I2C-HAT'
-    __labels = ['Rly1', 'Rly2', 'Rly3', 'Rly4', 'Rly5', 'Rly6', 'Rly7', 'Rly8', 'Rly9', 'Rly10']
-    
+
+    _BASE_ADDRESS = 0x50
+    _BOARD_NAME = 'Rly10 I2C-HAT'
+    _labels = ['Rly1', 'Rly2', 'Rly3', 'Rly4', 'Rly5', 'Rly6', 'Rly7', 'Rly8', 'Rly9', 'Rly10']
+
     def __init__(self, address):
-        I2CHat.__init__(self, address, self.__BASE_ADDRESS, self.__BOARD_NAME)
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
         self.cwdt = Cwdt(self)
-        self.do = DigitalOutputs(self, self.__labels)
+        self.do = DigitalOutputs(self, self._labels)
 
 class Di6Rly6(I2CHat):
     """This class exposes all operations supported by the Di6Rly6 I2C-HAT.
-    
+
     Args:
         address (:obj:`int`): I2C bus address, valid range is [0x60, 0x6F]
 
@@ -70,23 +70,45 @@ class Di6Rly6(I2CHat):
         cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
         di (:obj:`raspihats._digital.DigitalInputs`): provides access to DigitalInputs.
         do (:obj:`raspihats._digital.DigitalOutputs`): provides access to DigitalOutputs.
-    
+
     """
-    
-    __BASE_ADDRESS = 0x60
-    __BOARD_NAME = 'Di6Rly6 I2C-HAT'
-    __di_labels = ['Di1.1', 'Di1.2', 'Di1.3', 'Di1.4', 'Di1.5', 'Di1.6']
-    __do_labels = ['Rly1', 'Rly2', 'Rly3', 'Rly4', 'Rly5', 'Rly6']
-    
+
+    _BASE_ADDRESS = 0x60
+    _BOARD_NAME = 'Di6Rly6 I2C-HAT'
+    _di_labels = ['Di1.1', 'Di1.2', 'Di1.3', 'Di1.4', 'Di1.5', 'Di1.6']
+    _do_labels = ['Rly1', 'Rly2', 'Rly3', 'Rly4', 'Rly5', 'Rly6']
+
     def __init__(self, address):
-        I2CHat.__init__(self, address, self.__BASE_ADDRESS, self.__BOARD_NAME)
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
         self.cwdt = Cwdt(self)
-        self.di = DigitalInputs(self, self.__di_labels)
-        self.do = DigitalOutputs(self, self.__do_labels)
+        self.di = DigitalInputs(self, self._di_labels)
+        self.do = DigitalOutputs(self, self._do_labels)
+
+class DI16ac(I2CHat):
+    """This class exposes all operations supported by the DI16ac I2C-HAT.
+
+    Args:
+        address (:obj:`int`): I2C bus address, valid range is [0x40, 0x4F]
+
+    Attributes:
+        cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
+        di (:obj:`raspihats._digital.DigitalInputs`): provides access to DigitalInputs.
+
+    """
+
+    _BASE_ADDRESS = 0x40
+    _BOARD_NAME = 'DI16ac I2C-HAT'
+    _labels = ['I0', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13', 'I14', 'I15']
+
+
+    def __init__(self, address):
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
+        self.cwdt = Cwdt(self)
+        self.di = DigitalInputs(self, self._labels)
 
 class DQ16oc(I2CHat):
     """This class exposes all operations supported by the DQ16oc I2C-HAT.
-    
+
     Args:
         address (:obj:`int`): I2C bus address, valid range is [0x50, 0x5F]
 
@@ -94,12 +116,56 @@ class DQ16oc(I2CHat):
         cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
         do (:obj:`raspihats._digital.DigitalOutputs`): provides access to DigitalOutputs.
     """
-    
-    __BASE_ADDRESS = 0x50    
-    __BOARD_NAME = 'DQ16oc I2C-HAT'
-    __labels = ['Q0', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 'Q15']
-    
+
+    _BASE_ADDRESS = 0x50
+    _BOARD_NAME = 'DQ16oc I2C-HAT'
+    _labels = ['Q0', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 'Q15']
+
     def __init__(self, address):
-        I2CHat.__init__(self, address, self.__BASE_ADDRESS, self.__BOARD_NAME)
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
         self.cwdt = Cwdt(self)
-        self.do = DigitalOutputs(self, self.__labels)
+        self.do = DigitalOutputs(self, self._labels)
+
+class DQ10rly(I2CHat):
+    """This class exposes all operations supported by the DQ10rly I2C-HAT.
+
+    Args:
+        address (:obj:`int`): I2C bus address, valid range is [0x50, 0x5F]
+
+    Attributes:
+        cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
+        do (:obj:`raspihats._digital.DigitalOutputs`): provides access to DigitalOutputs.
+    """
+
+    _BASE_ADDRESS = 0x50
+    _BOARD_NAME = 'DQ10rly I2C-HAT'
+    _labels = ['Q0', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9']
+
+    def __init__(self, address):
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
+        self.cwdt = Cwdt(self)
+        self.do = DigitalOutputs(self, self._labels)
+
+class DI6acDQ6rly(I2CHat):
+    """This class exposes all operations supported by the DI6acDQ6rly I2C-HAT.
+
+    Args:
+        address (:obj:`int`): I2C bus address, valid range is [0x60, 0x6F]
+
+    Attributes:
+        cwdt (:obj:`raspihats._i2c_hat.Cwdt`): provides access to CommunicationWatchDogTimer.
+        di (:obj:`raspihats._digital.DigitalInputs`): provides access to DigitalInputs.
+        do (:obj:`raspihats._digital.DigitalOutputs`): provides access to DigitalOutputs.
+
+    """
+
+    _BASE_ADDRESS = 0x60
+    _BOARD_NAME = 'DI6acDQ6rly I2C-HAT'
+    _di_labels = ['I0', 'I1', 'I2', 'I3', 'I4', 'I5']
+    _do_labels = ['Q0', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5']
+
+    def __init__(self, address):
+        I2CHat.__init__(self, address, self._BASE_ADDRESS, self._BOARD_NAME)
+        self.cwdt = Cwdt(self)
+        self.di = DigitalInputs(self, self._di_labels)
+        self.do = DigitalOutputs(self, self._do_labels)
