d7205cbbd65d63b58e5baa7a1b3b3a9afe5061f1
Florin Costa <florin.costa83@gmail.com>
2017-02-17 22:11:06 +0200

    v2.0.1, fixed clock stretching timeout script

--- clk_stretch/clk_stretch.py
+++ clk_stretch/clk_stretch.py
@@ -1,75 +1,81 @@
-from subprocess import call
-from platform import system, linux_distribution
+import os
+import sys
+import subprocess
+import traceback
+import platform
 
+def build(set_exec_name, get_exec_name):
+    print("Building i2c1 clock stretch timeout setter and getter")
+    subprocess.check_output(["gcc", "-o", set_exec_name, "i2c1_set_clkt_tout.c"])
+    subprocess.check_output(["gcc", "-o", get_exec_name, "i2c1_get_clkt_tout.c"])
 
-def rpi_clk_stretch_timeout(clk_stretch_timeout, baudrate):
-    dest_path = '/usr/local/bin/'
-    set_exec_name = 'i2c1_set_clkt_tout'
-    get_exec_name = 'i2c1_get_clkt_tout'
+def copy(set_exec_name, get_exec_name, dest_path):
+    print("Copying i2c1 clock stretch timeout setter and getter to " + dest_path)
+    subprocess.check_output(["mv", set_exec_name, dest_path])
+    subprocess.check_output(["mv", get_exec_name, dest_path])
+
+def set_clk_stretch_timeout(set_exec_name, get_exec_name, clkt_tout):
+    print("Setting i2c1 clock stretch timeout value to " + str(clkt_tout))
+    subprocess.check_output([set_exec_name, str(clkt_tout)])
+    print("Checking i2c1 clock stretch timeout value")
+    output = subprocess.check_output([get_exec_name])
+    if type(output) != str:
+        output = output.decode("utf-8")
+    if str(clkt_tout) not in output:
+        raise Exception("Failed to set clock stretch timeout value! " + output)
+
+def update_rc_local(dest_path, set_exec_name, clkt_tout):
+    exit_token = "exit 0"
+    clks_line = dest_path + set_exec_name + " " + str(clkt_tout)
+    updated = False
+    with open("/etc/rc.local", "r+") as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            if set_exec_name in lines[i]:
+                print("rc.local already updated")
+                updated = True
+                break
+            if lines[i].startswith(exit_token):
+                lines[i] = lines[i].replace(exit_token, clks_line + "\n\n" + exit_token + "\n")
+                # return pointer to top of file so we can re-write the content with replaced string
+                f.seek(0)
+                # clear file content
+                f.truncate()
+                # re-write the content with the updated content
+                f.write("".join(lines))
+                print("rc.local updated")
+                updated = True
+                break
+    if not updated:
+        raise Exception("Failed to update rc.local!")
+
+def setup_clk_stretch_timeout(clk_stretch_timeout, baudrate):
+    dest_path = "/usr/local/bin/"
+    set_exec_name = "i2c1_set_clkt_tout"
+    get_exec_name = "i2c1_get_clkt_tout"
     clkt_tout = int(float(clk_stretch_timeout) * float(baudrate))
-    line_to_add = dest_path + set_exec_name + " " + str(clkt_tout)
-    
-    print("Building i2c1 clock stretch setter and getter.")
-    call(["gcc","-o", set_exec_name, "i2c1_set_clkt_tout.c"])
-    call(["gcc","-o", get_exec_name, "i2c1_get_clkt_tout.c"])
-    
-    print("Copying i2c1 clock stretch setter and getter to " + dest_path + ".")
-    call(["mv",set_exec_name, dest_path])
-    call(["mv",get_exec_name, dest_path])
-    
-    print("Setting i2c1 clock stretch value.")
-    call([set_exec_name, clkt_tout])
-    
-    print("Checking i2c1 clock stretch value.")
-    call([get_exec_name])
-    
-    print("Udating rc.local.")
-    # open file with r+b (allow write and binary mode)
-    f = open('/etc/rc.local', 'r+b')   
-    # read entire content of file into memory
-    f_lines = f.readlines()
-    
-    exit_token = 'exit 0'
-    rc_local_updated = False
-    for i in range(0, len(f_lines)):
-        if set_exec_name in f_lines[i]:
-            f_lines[i] = line_to_add + "\n"
-            rc_local_updated = True
-            break
-        if exit_token in f_lines[i]:
-            f_lines[i] = line_to_add + "\n\n" + exit_token + "\n"
-            rc_local_updated = True
-            break
-        
-    if not rc_local_updated:
-        print ("Failed to update rc.local!!!")
-    
-    # return pointer to top of file so we can re-write the content with replaced string
-    f.seek(0)
-    # clear file content 
-    f.truncate()
-    # re-write the content with the updated content
-    f.write("".join(f_lines))
-    # close file
-    f.close()
+
+    build(set_exec_name, get_exec_name)
+    copy(set_exec_name, get_exec_name, dest_path)
+    set_clk_stretch_timeout(set_exec_name, get_exec_name, clkt_tout)
+    update_rc_local(dest_path, set_exec_name, clkt_tout)
 
 def main():
     RASPBERRY_PI = 'raspberry-pi'
-    
     PLATFORM = 'Unknown'
-    if system().lower() == 'linux':
-        if linux_distribution()[0].lower() == 'debian':
+    if platform.system().lower() == 'linux':
+        if platform.linux_distribution()[0].lower() == 'debian':
             try:
                 with open('/proc/cpuinfo') as f:
                     for line in f:
                         line = line.strip()
                         if line.startswith('Hardware') and ('BCM2708' in line or 'BCM2709' in line):
                             PLATFORM = RASPBERRY_PI
-                            rpi_clk_stretch_timeout(clk_stretch_timeout=0.2, baudrate=100000)
+                            setup_clk_stretch_timeout(clk_stretch_timeout=0.2, baudrate=100000)
                             break
             except:
-                pass
-    
+                traceback.print_exc()
+
     LEGAL_PLATFORMS = RASPBERRY_PI
     assert PLATFORM in LEGAL_PLATFORMS, "Don't understand platform"
 
